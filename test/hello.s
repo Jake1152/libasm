7d2a3fd38275be497486257aa2c44e6ee8177bf1
Jake1152 <rancho974@gmail.com>
2024-12-22 20:03:30 +0900

    test: print hello world

--- test/hello.s
+++ test/hello.s
@@ -1,5 +1,5 @@
 section .data
-    hello db 'Hello', ' 42 ', ', World!',0XA   ;  db => define byte asm에서 
+    hello db 'Hello Min-jo!', 0XA   ;  db => define byte asm에서 
                                     ; 바이트 단위로 저장하는 명령어 
                                     ; 0XA => \n
     hello_len equ $ - hello         ; 메시지 길이 계산
@@ -12,10 +12,9 @@ _start:
     ; stdout으로 메시지 write하기
     mov rax, 1          ; syscall: write
     mov rdi, 1          ; file descriptor: stdout
-    mov rsi, hello      ; pointer to message
+    mov rsi, hello        ; pointer to message
     mov rdx, hello_len  ; message length
     syscall             ; make the syscall
-
 
     ; Exit the program
     mov rax, 60         ; syscall: exit
