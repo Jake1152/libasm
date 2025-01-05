d4edff134337ed1d605d17b248938103d1d7fecb
Jake1152 <rancho974@gmail.com>
2025-01-05 19:51:47 +0900

    fix: read, errno setting and calling convention errno

--- srcs/ft_read.s
+++ srcs/ft_read.s
@@ -11,29 +11,49 @@ section .text
 ; 1. errno_location1
 ; 2. calling convention saved by
 ; ref: https://die4taoam.tistory.com/37
-; calling_convention saved
+; ## calling_convention saved register
+; ### caller
+; rax, rcx, rdx, rsi, rdi, r8, r9, r10, r11
+
+; ### callee
+; rbx, rbp, r12, r13, r14, r15
 
 ; calling convention register 저장
-; save_register_of_caller:
+save_register_of_caller:
+	mov rbx, rax
 
-;
-; restore_register_of_caller:
+restore_register_of_caller:
+	mov rax, rbx
+
+; save_register_of_callee:
+
+; restore_register_of_callee:
 	
 
 handle_error:
-	call __errno_location wrt ..plt
+	; errno값을 다른 곳에 저장
+	mov rbx, rax
+	@ call save_register_of_caller
+
+
+	call __errno_location wrt ..plt ; 상대주소로 errno_location 호출하기 위함.
 	mov rax, -1
-;- rsi
-;- rdi
-;- rdx
+
+	call restore_register_of_caller:
+
+; read syscall에서 쓰는 파라미터 rdi, rsi, rdx
 ft_read:
 	mov rax, 0
+
+	
 	syscall
 
-	; # Mac jc로 carry flag변화를 감지
+
+	; # Mac, jc로 carry flag변화를 감지
 	; jc handle_error
-	; linux version
+	; # Linux, version
 	cmp rax, 0
+
 	jl handle_error
 
 	ret
