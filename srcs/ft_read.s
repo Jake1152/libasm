4ee1500f2f550bccbe000406b0f6d179b308c600
Jake1152 <rancho974@gmail.com>
2025-01-05 16:39:28 +0900

    wip: ft_read
    - TODO: calling convention

---
+++ srcs/ft_read.s
@@ -0,0 +1,39 @@
+section .text
+	global ft_read
+	extern __errno_location
+
+
+;ssize_t ft_read(int fd, void *buf, size_t count);
+; Mac, Linux
+;	rsi, rdi, rdx, rcx, r8, r9
+
+; # 고려사항 
+; 0. 정상 리턴 케이스
+; 1. errno_location
+; 2. calling convention saved by
+; ref: https://die4taoam.tistory.com/37
+; calling_convention saved
+
+;
+save_register_of_caller:
+
+
+;
+restore_register_of_caller:
+
+
+handle_error:
+	call __errno_location	; Mac은 ___error (!!언더바 3개!!)
+	mov rax, -1
+
+;- rsi
+;- rdi
+;- rdx
+ft_read:
+	mov rax, 0
+	syscall
+	; # Mac jc로 carry flag변화를 감지
+	; jc handle_error
+	; linux version
+	cmp rax, 0
+	jl handle_error
